6d84a1b936631364556bd00c6250930ae06dc2ea
potichatmignon <arthur.garnier1090@gmail.com>
2025-07-17 00:30:47 +0200

    add: basic app structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./pages/HomePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Docs from "./pages/DocsPage";
+import NotFound from "./pages/NotFoundPage";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/docs",
+    element: <Docs />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <div>NotFoundPage</div>
+      <Link to="/">Go to Home</Link>
+    </>
+  );
+};
+
+export default NotFoundPage;
